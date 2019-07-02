148bce9cba38ba19687ec6cdf411e59b2714ec3d
njevtic22 <njevtic22@gmail.com>
2019-07-02 13:20:31 +0200

    Zapocete medote za ucitavanje kupaca i korisnickih imena.

--- globus prototype/src/main/FileLoader.java
+++ globus prototype/src/main/FileLoader.java
@@ -1,6 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+
+import model.RegistrovaniKupac;
+
 public class FileLoader {
-	//Here we will load all objects
+	public void loadRegUsers(ArrayList<RegistrovaniKupac> users) {
+		//TODO: Load all registered users in list users.
+		
+	}
+	
+	public void loadUsername(ArrayList<RegistrovaniKupac> users, ArrayList<String> usernames) {
+		//TODO: WE maybe don't need this method because we can read from 'users'.
+		//		This way is maybe faster.
+	}
 
 }
